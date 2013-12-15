531b2e525a27665b239eee8e09b3bf4ab57c141d
Sean Bell <sbell@cs.cornell.edu>
2013-12-15 06:32:22 -0500

    updated demo

--- setup-demo.sh
+++ setup-demo.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# install dependencies
+# install python dependencies: django, django-compressor, ua-parser
 sudo pip install "django>=1.4.3,<1.5" "django-compressor>=1.2,<2" "ua-parser>=0.3.2,<0.4"
 
-# update STATIC_ROOT in the local project settings
+# In the settings file (example_project/example_project/settings.py),
+# automatically update the STATIC_ROOT variable to point to a temporary
+# directory in the current repository.  It doesn't matter what the
+# directory is, as long as it's writable and previously unused.
 STATIC_ROOT=$(pwd)/tmp-static/
 mkdir -p $STATIC_ROOT
 sed -r -i -e "s|^\s*STATIC_ROOT\s*=.*$|STATIC_ROOT = '$STATIC_ROOT'|" \
