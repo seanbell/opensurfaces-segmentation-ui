3360e0f57a0429cb182233962032f012b3538434
Sean Bell <sbell@cs.cornell.edu>
2013-12-16 06:53:13 -0500

    bugfix -- thank you Tsung-Yi Lin

--- python-run-demo.sh
+++ python-run-demo.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 
 SRC_DIR=example_project/segmentation/static/
+JS_DIR=$SRC_DIR/js
 BUILD_DIR=static
 
 mkdir -p $BUILD_DIR/js $BUILD_DIR/css $BUILD_DIR/img
 
+# the order in which they are concatenated is important
 echo "Merging javascript..."
-rm -f static/js/build-js.js
-# the order in which they are concatenated is very important
-for f in jquery-1.8.2.min.js jquery-ui-1.9.1.custom.min.js bootstrap-2.2.1.min.js json2.js csrf.js jquery.hotkeys.js kinetic-v4.3.3.min.js delaunay.js; do
-	cat $SRC_DIR/js/lib/$f >> static/js/build-js.js
-done
+cat $JS_DIR/lib/jquery-1.8.2.min.js \
+	$JS_DIR/lib/jquery-ui-1.9.1.custom.min.js \
+	$JS_DIR/lib/bootstrap-2.2.1.min.js \
+	$JS_DIR/lib/json2.js \
+	$JS_DIR/lib/csrf.js \
+	$JS_DIR/lib/jquery.hotkeys.js \
+	$JS_DIR/lib/kinetic-v4.3.3.min.js \
+	$JS_DIR/lib/delaunay.js \
+	> static/js/build-js.js
 
+# the order in which these files are concatenated is important
 echo "Compiling coffeescript..."
-cffee --bare --join static/js/build-coffee.js --compile $(find $SRC_DIR -name '*.coffee')
+coffee --bare \
+	--join static/js/build-coffee.js \
+	--compile \
+		$JS_DIR/common/hacks.coffee \
+		$JS_DIR/common/modals.coffee \
+		$JS_DIR/common/scroll.coffee \
+		$JS_DIR/common/ui_events.coffee \
+		$JS_DIR/common/util.coffee \
+		$JS_DIR/common/geom.coffee \
+		$JS_DIR/common/actions.coffee \
+		$JS_DIR/common/active_timer.coffee \
+		$JS_DIR/common/get_url_params.coffee \
+		$JS_DIR/poly/actions.coffee \
+		$JS_DIR/poly/controller_state.coffee \
+		$JS_DIR/poly/controller_ui.coffee \
+		$JS_DIR/poly/stage_ui.coffee \
+		$JS_DIR/poly/polygon_ui.coffee \
+		$JS_DIR/mturk/mt_submit.coffee \
+		$JS_DIR/mturk/mt_segment_material.coffee
 
 echo "Copying jss, css, img..."
 cp -r $SRC_DIR/css $SRC_DIR/img static
