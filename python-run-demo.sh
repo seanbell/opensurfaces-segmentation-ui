c0caddbd2edf79464216158fe4dbe9fd73c6b0d8
Sean Bell <sbell@cs.cornell.edu>
2013-12-16 19:00:33 -0500

    updated build process for non-django version

--- python-run-demo.sh
+++ python-run-demo.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
 SRC_DIR=example_project/segmentation/static/
-JS_DIR=$SRC_DIR/js
 BUILD_DIR=static
 
-mkdir -p $BUILD_DIR/js $BUILD_DIR/css $BUILD_DIR/img
+JS_DIR=$SRC_DIR/js
+CSS_DIR=$SRC_DIR/css
 
-# the order in which they are concatenated is important
-echo "Merging javascript..."
-cat $JS_DIR/lib/jquery-1.8.2.min.js \
-	$JS_DIR/lib/jquery-ui-1.9.1.custom.min.js \
-	$JS_DIR/lib/bootstrap-2.2.1.min.js \
-	$JS_DIR/lib/json2.js \
-	$JS_DIR/lib/csrf.js \
-	$JS_DIR/lib/jquery.hotkeys.js \
-	$JS_DIR/lib/kinetic-v4.3.3.min.js \
-	$JS_DIR/lib/delaunay.js \
-	> static/js/build-js.js
+mkdir -p \
+	$BUILD_DIR/js \
+	$BUILD_DIR/css \
+	$BUILD_DIR/img
 
-# the order in which these files are concatenated is important
+# the order in which these files are compiled is important
 echo "Compiling coffeescript..."
 coffee --bare \
-	--join static/js/build-coffee.js \
+	--join $BUILD_DIR/js/build-coffee.js \
 	--compile \
 		$JS_DIR/common/hacks.coffee \
 		$JS_DIR/common/modals.coffee \
@@ -40,8 +33,31 @@ coffee --bare \
 		$JS_DIR/mturk/mt_submit.coffee \
 		$JS_DIR/mturk/mt_segment_material.coffee
 
-echo "Copying css, img..."
-cp -r $SRC_DIR/css $SRC_DIR/img static
+# concatenate javascript libraries with the compiled coffeescript
+# (the order in which they are concatenated is important)
+echo "Merging javascript..."
+cat $JS_DIR/lib/jquery-1.8.2.min.js \
+	$JS_DIR/lib/jquery-ui-1.9.1.custom.min.js \
+	$JS_DIR/lib/bootstrap-2.2.1.min.js \
+	$JS_DIR/lib/json2.js \
+	$JS_DIR/lib/csrf.js \
+	$JS_DIR/lib/jquery.hotkeys.js \
+	$JS_DIR/lib/kinetic-v4.3.3.min.js \
+	$JS_DIR/lib/delaunay.js \
+	$BUILD_DIR/js/build-coffee.js \
+	> $BUILD_DIR/js/opensurfaces-segmentation-ui.js
+
+# cleanup
+rm $BUILD_DIR/js/build-coffee.js
+
+echo "Merging css..."
+cat $CSS_DIR/jquery-ui-1.9.1.custom.min.css \
+	$CSS_DIR/bootstrap-2.2.1.min.css \
+	$CSS_DIR/mturk.css \
+	> $BUILD_DIR/css/opensurfaces-segmentation-ui.css
+
+echo "Copying images"
+cp -r $SRC_DIR/img $BUILD_DIR
 
 echo "Running webserver..."
 python2.7 -m SimpleHTTPServer
